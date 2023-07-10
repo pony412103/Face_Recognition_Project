fee4afb20ca4a57c31e246819d888e8f285a7f97
PonyWeng <80569901+PonyWeng@users.noreply.github.com>
2023-07-10 13:54:42 +0800

    Add files via upload
    First Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 17:42:55 2023
+
+@author: pony7
+"""
+
+import pyautogui
+print("測試")
+
+pyautogui.typewrite(['5','j','/',' ','v','u','/',' ','u','i','0','4','n','3',' ','1','l',' ','r','u','/',' ','enter'],'0.25')
+
+
+a=[1,2,3]
+b=['A','B','C']
+c=['鐘興彥死包莖']
+print(a)
+print(b)
+
+
+print(a[0])
+print(a[1])
+print(a[2])
+
+
+print(b[0])
+print(b[1])
+print(b[2])
+
+print(c)
+print(c[0])
