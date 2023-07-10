6072439228851b4a75855b9c221514b9f0875057
pony71909 <pony71909@gmail.com>
2023-07-10 14:05:56 +0800

    MyFirstCommit
    更改程式碼

--- main.py
+++ main.py
@@ -5,27 +5,7 @@ Created on Wed Jun 28 17:42:55 2023
 @author: pony7
 """
 
-import pyautogui
-print("測試")
 
-pyautogui.typewrite(['5','j','/',' ','v','u','/',' ','u','i','0','4','n','3',' ','1','l',' ','r','u','/',' ','enter'],'0.25')
+import cv2
 
-
-a=[1,2,3]
-b=['A','B','C']
-c=['鐘興彥死包莖']
-print(a)
-print(b)
-
-
-print(a[0])
-print(a[1])
-print(a[2])
-
-
-print(b[0])
-print(b[1])
-print(b[2])
-
-print(c)
-print(c[0])
+print("HelloWorld")
